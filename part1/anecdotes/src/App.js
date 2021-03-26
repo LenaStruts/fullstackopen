c22ad965281cda75ead3690304933a5953dfad67
LenaStruts <lena19struts@gmail.com>
2021-03-26 11:56:56 +0100

    1.13*, 1.14*: anecdotes step2, step3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
@@ -6,6 +6,13 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const Anecdote = ({ header, anecdote }) => (
+  <>
+    <h1>{header}</h1>
+    <p>{anecdote}</p>
+  </>
+)
+  
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -14,21 +21,33 @@ const App = () => {
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ]
+  ];
    
-  const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
+  const i = votes.findIndex(vote => vote === Math.max.apply(null, votes))
+
+  const vote = () => {
+    const newVotes = [ ...votes ]
+    newVotes[selected] += 1
+    console.log(newVotes)
+    setVotes(newVotes)
+  }
+  
   const getRandomAnecdote = () => {
-    let randomN = Math.floor(Math.random() * anecdotes.length);
-    setSelected(randomN);
+    let randomN = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomN)
   }
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
+      <Anecdote header='Anecdote of the day' anecdote={anecdotes[selected]} />
       <Button handleClick={getRandomAnecdote} text='next anecdote' />
+      <Button handleClick={vote} text='vote' />
+      <Anecdote header='Anecdotes with most votes' anecdote={anecdotes[i]} />
     </div>
   )
 }
 
-export default App;
+export default App
