96baa8c5e55699f82a9a157ae1c2ec15d7d057e7
LenaStruts <lena19struts@gmail.com>
2021-04-06 14:18:55 +0200

    2.2, 2.3*: Course information step7, step8

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -5,6 +5,7 @@ const Course = ({ course }) => {
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
@@ -15,12 +16,13 @@ const Header = ({ course }) => {
   )
 }
 
-// const Total = ({ course }) => {
-//   const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-//   return(
-//     <p>Number of exercises {sum}</p>
-//   ) 
-// }
+const Total = ({ course }) => {
+  const sum = course.parts.map(course => course.exercises)
+  .reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+  return (
+    <p><strong>total of {sum} exercises</strong></p>
+  ) 
+}
 
 const Part = ({ part }) => {
   return (
@@ -59,7 +61,12 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
-      }
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      },
     ]
   }
 
